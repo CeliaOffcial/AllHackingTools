cb3f32d947c97590f4979b3cd5079c665c73279e
CeliaOfficial <161849322+CeliaOffcial@users.noreply.github.com>
2024-03-18 14:54:25 +0700

    Update Logo.sh

--- Logo.sh
+++ Logo.sh
@@ -13,6 +13,7 @@ echo '
  |  ╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░╚══╝░╚═════╝░░░░░░░░░░╚═╝░░░░╚════╝░░╚════╝░╚══════╝ v2.7.4
 [3] Title    : AllHackingTools - Tool for hacking  -  ⁣ATTENTION! The author of this article is not respo-
 [3] Github   : https://github.com/CeliaOffcial  -  nsi⁣ble responsible for any consequences of reading it. 
-[2] Coded By : Celia Official (Clone)  -  All materials are provided for educational purposes only! 
-[4]——————-——-–—————-—-–—————-————-—–————-———-—–—————————-——–—————–-——[715]
+[2] Coded By : Celia Official (Clone)  -  All materials are provided for educational purposes only!
+[4] Website : https://celiapanellstore.my.id  - Website Owner @CeliaOffcial (Otakudesu.io)
+[5]——————-——-–—————-—-–—————-————-—–————-———-—–—————————-——–—————–-——[715]
 '|lolcat -p 1.6 -a -d 2 -s 20.0
