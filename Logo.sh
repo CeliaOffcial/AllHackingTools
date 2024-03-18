f6e1a818aaf9b558977df77804edb725d69be08d
CeliaOfficial <161849322+CeliaOffcial@users.noreply.github.com>
2024-03-18 11:14:39 +0700

    Update Logo.sh

--- Logo.sh
+++ Logo.sh
@@ -1,7 +1,7 @@
 #!/usr/bin
 #Copyright 2021 AllHackingTools
-#Written by : Misha Korzhik
-#Github     : http://github.com/mishakorzik
+#Written by : Celia Official
+#Github     : http://github.com/CeliaOffcial
 
 echo '
 [1]
@@ -12,7 +12,7 @@ echo '
  |  ██║░░██║██║░░██║╚█████╔╝██║░╚██╗██║██║░╚███║╚██████╔╝░░░░░░░░░██║░░░╚█████╔╝╚█████╔╝███████╗
  |  ╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░╚══╝░╚═════╝░░░░░░░░░░╚═╝░░░░╚════╝░░╚════╝░╚══════╝ v2.7.4
 [3] Title    : AllHackingTools - Tool for hacking  -  ⁣ATTENTION! The author of this article is not respo-
-[3] Github   : https://github.com/mishakorzik  -  nsi⁣ble responsible for any consequences of reading it. 
-[2] Coded By : Misha Korzhik (Міша Коржик)  -  All materials are provided for educational purposes only! 
+[3] Github   : https://github.com/CeliaOffcial  -  nsi⁣ble responsible for any consequences of reading it. 
+[2] Coded By : Celia Official (Clone)  -  All materials are provided for educational purposes only! 
 [4]——————-——-–—————-—-–—————-————-—–————-———-—–—————————-——–—————–-——[715]
 '|lolcat -p 1.6 -a -d 2 -s 20.0
